b0573042fd298bad8ff184f4e0048acdc57fff17
tomr <tomryanx@gmail.com>
2019-08-20 18:30:47 +1000

    first pass at modify spec to match changes

--- spec/filters/clone_spec.rb
+++ spec/filters/clone_spec.rb
@@ -10,19 +10,24 @@ describe LogStash::Filters::Clone do
     config <<-CONFIG
       filter {
         clone {
-          clones => ["clone", "clone", "clone"]
+          clones => {
+            "type" => "clone"
+            "clone" => "clone1"
+          }
         }
       }
     CONFIG
 
     sample("message" => "hello world", "type" => "original") do
-      insist { subject }.is_a? Array
-      insist { subject.length } == 4
+      insist { subject }.is_a? Hash
+      insist { subject.length } == 3
       subject.each_with_index do |s,i|
         if i == 0 # last one should be 'original'
           insist { s.get("type") } == "original"
-        else
+        else if i = 1
           insist { s.get("type")} == "clone"
+        else
+          insist { s.get("clone")} == "clone1"
         end
         insist { s.get("message") } == "hello world"
       end
@@ -33,7 +38,10 @@ describe LogStash::Filters::Clone do
     config <<-CONFIG
       filter {
         clone {
-          clones => ["nginx-access-clone1", "nginx-access-clone2"]
+          clones => {
+            "clone" => "nginx-access-clone1"
+            "type" => "nginx-access-clone2"
+          }
           add_tag => ['RABBIT','NO_ES']
           remove_tag => ["TESTLOG"]
         }
@@ -50,7 +58,7 @@ describe LogStash::Filters::Clone do
       reject { subject[0].get("tags") }.include? "RABBIT"
       reject { subject[0].get("tags") }.include? "NO_ES"
       #All clones go through filter_matched
-      insist { subject[1].get("type") } == "nginx-access-clone1"
+      insist { subject[1].get("clone") } == "nginx-access-clone1"
       reject { subject[1].get("tags") }.include? "TESTLOG"
       insist { subject[1].get("tags") }.include? "RABBIT"
       insist { subject[1].get("tags") }.include? "NO_ES"
@@ -67,7 +75,7 @@ describe LogStash::Filters::Clone do
     config <<-CONFIG
       filter {
         clone {
-          clones => [ 'clone1' ]
+          clones => { 'clone1' => 'clone1' }
         }
       }
     CONFIG
