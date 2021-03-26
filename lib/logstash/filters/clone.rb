523e521dafcece79ae04cdad2cad6ba746592d9b
kaisecheng <69120390+kaisecheng@users.noreply.github.com>
2021-03-26 22:23:47 +0100

    add ECS support (#24)
    ECS add clone type to `tags` while legacy keeps `type` in root level
    
    Fixed: #21

--- lib/logstash/filters/clone.rb
+++ lib/logstash/filters/clone.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require "logstash/filters/base"
 require "logstash/namespace"
+require "logstash/plugin_mixins/ecs_compatibility_support"
 
 # The clone filter is for duplicating events.
 # A clone will be created for each type in the clone list.
@@ -8,7 +9,12 @@ require "logstash/namespace"
 # Created events are inserted into the pipeline 
 # as normal events and will be processed by the remaining pipeline configuration 
 # starting from the filter that generated them (i.e. this plugin).
+#
+# ECS disabled: set the value of the root-level `type` (undefined in ECS) of each resulting event
+# to one of the values provided in its `clones` directive.
+# ECS enabled: add a `tags` of each resulting event to one of the values provided in its `clones` directive.
 class LogStash::Filters::Clone < LogStash::Filters::Base
+  include LogStash::PluginMixins::ECSCompatibilitySupport(:disabled, :v1)
 
   config_name "clone"
 
@@ -16,6 +22,12 @@ class LogStash::Filters::Clone < LogStash::Filters::Base
   config :clones, :validate => :array, :required => true
 
   public
+
+  def initialize(*params)
+    super
+    @enhance_clone_type_method = method( ecs_select[disabled: :set_event_type, v1: :add_event_tag] )
+  end
+
   def register
     logger.warn("The parameter 'clones' is empty, so no clones will be created.") if @clones.empty?
   end
@@ -24,13 +36,25 @@ class LogStash::Filters::Clone < LogStash::Filters::Base
   def filter(event)
     @clones.each do |type|
       clone = event.clone
-      clone.set("type", type)
+      @enhance_clone_type_method.(clone, type)
       filter_matched(clone)
       @logger.debug("Cloned event", :clone => clone, :event => event)
 
       # Push this new event onto the stack at the LogStash::FilterWorker
       yield clone
     end
+  end
+
+  def set_event_type(event, type)
+    event.set("type", type)
+    event
+  end
+
+  def add_event_tag(event, clone_type)
+    tags = Array(event.get("tags"))
+    tags << clone_type
+    event.set("tags", tags)
+    event
   end
 
 end # class LogStash::Filters::Clone
--- spec/filters/clone_spec.rb
+++ spec/filters/clone_spec.rb
@@ -1,92 +1,122 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/filters/clone"
+require 'logstash/plugin_mixins/ecs_compatibility_support/spec_helper'
 
 describe LogStash::Filters::Clone do
 
-  describe "#filter" do
-    subject { described_class.new(settings) }
-    let(:event) { LogStash::Event.new(input) }
-    before(:each) do
-      subject.register
-    end
-    describe "all defaults" do
-      let(:input) { { "message" => "hello world", "type" => "original" } }
-      let(:settings) { { "clones" => ["clone", "clone", "clone"] } }
+  describe "#filter", :ecs_compatibility_support, :aggregate_failures do
+    ecs_compatibility_matrix(:disabled, :v1) do |ecs_select|
 
-      it "should generate new clones + current event" do
-        events = [event]
-        subject.filter(event) {|e| events << e }
-        expect(events.length).to eq(4)
-        events.each_with_index do |s,i|
-          if i == 0 # last one should be 'original'
-            expect(s.get("type")).to eq("original")
-          else
-            expect(s.get("type")).to eq("clone")
+      subject { described_class.new(settings) }
+      let(:event) { LogStash::Event.new(input) }
+      before(:each) do
+        allow_any_instance_of(described_class).to receive(:ecs_compatibility).and_return(ecs_compatibility)
+        subject.register
+      end
+
+      describe "all defaults" do
+        let(:input) { { "message" => "hello world", "type" => "original" } }
+        let(:settings) { { "clones" => ["clone", "clone", "clone"] } }
+
+        it "should generate new clones + current event" do
+          events = [event]
+          subject.filter(event) {|e| events << e }
+          expect(events.length).to eq(4)
+          events.each_with_index do |s,i|
+            if ecs_compatibility == :disabled
+              if i == 0 # last one should be 'original'
+                expect(s.get("type")).to eq("original")
+              else
+                expect(s.get("type")).to eq("clone")
+              end
+              expect(s.get("message")).to eq("hello world")
+            else
+              expect(s.get("type")).to eq("original")
+              expect(s.get("message")).to eq("hello world")
+              expect(s.get("tags")).to include("clone") unless i == 0
+            end
           end
-          expect(s.get("message")).to eq("hello world")
+
+        end
+      end
+
+      describe "Complex use" do
+        let(:settings) do
+          { "clones" => ["nginx-access-clone1", "nginx-access-clone2"],
+            "add_tag" => ['RABBIT','NO_ES'],
+            "remove_tag" => ["TESTLOG"]
+          }
+        end
+        let(:input) { { "type" => "nginx-access", "tags" => ["TESTLOG"], "message" => "hello world" } }
+
+        it "works as expected" do
+          events = [event]
+          subject.filter(event) {|e| events << e }
+          expect(events.length).to eq(3)
+
+          if ecs_compatibility == :disabled
+            expect(events[0].get("type")).to eq("nginx-access")
+            #Initial event remains unchanged
+            expect(events[0].get("tags")).to include("TESTLOG")
+            expect(events[0].get("tags")).to_not include("RABBIT")
+            expect(events[0].get("tags")).to_not include("NO_ES")
+            #All clones go through filter_matched
+            expect(events[1].get("type")).to eq("nginx-access-clone1")
+            expect(events[1].get("tags")).to_not include("TESTLOG")
+            expect(events[1].get("tags")).to include("RABBIT")
+            expect(events[1].get("tags")).to include("NO_ES")
+
+            expect(events[2].get("type")).to eq("nginx-access-clone2")
+            expect(events[2].get("tags")).to_not include("TESTLOG")
+            expect(events[2].get("tags")).to include("RABBIT")
+            expect(events[2].get("tags")).to include("NO_ES")
+          else
+            expect(events[0].get("type")).to eq("nginx-access")
+            expect(events[0].get("tags")).to include("TESTLOG")
+            expect(events[0].get("tags")).to_not include("RABBIT")
+            expect(events[0].get("tags")).to_not include("NO_ES")
+
+            (1..2).each do |i|
+              expect(events[i].get("type")).to eq("nginx-access")
+              expect(events[i].get("tags")).to_not include("TESTLOG")
+              expect(events[i].get("tags")).to include("RABBIT")
+              expect(events[i].get("tags")).to include("NO_ES")
+            end
+
+            expect(events[1].get("tags")).to include("nginx-access-clone1")
+            expect(events[2].get("tags")).to include("nginx-access-clone2")
+          end
+        end
+      end
+
+      describe "Bug LOGSTASH-1225" do
+        ### LOGSTASH-1225: Cannot clone events containing numbers.
+        let(:settings) { { "clones" => [ 'clone1' ] } }
+        let(:input) { { "type" => "bug-1225", "message" => "unused", "number" => 5 } }
+
+        it "clones events containing numbers" do
+          events = [event]
+          subject.filter(event) {|e| events << e }
+          expect(events[0].get("number")).to eq(5)
+          expect(events[1].get("number")).to eq(5)
         end
       end
     end
 
-    describe "Complex use" do
-      let(:settings) do
-        { "clones" => ["nginx-access-clone1", "nginx-access-clone2"],
-          "add_tag" => ['RABBIT','NO_ES'],
-          "remove_tag" => ["TESTLOG"]
-        }
+    describe "#register" do
+      context "when clones is an empty array" do
+        subject { described_class.new("clones" => []) }
+        it "logs a warning" do
+          expect(subject.logger).to receive(:warn)
+          expect { subject.register }.to_not raise_error
+        end
       end
-      let(:input) { { "type" => "nginx-access", "tags" => ["TESTLOG"], "message" => "hello world" } }
-
-      it "works as expected" do
-        events = [event]
-        subject.filter(event) {|e| events << e }
-        expect(events.length).to eq(3)
-
-        expect(events[0].get("type")).to eq("nginx-access")
-        #Initial event remains unchanged
-        expect(events[0].get("tags")).to include("TESTLOG")
-        expect(events[0].get("tags")).to_not include("RABBIT")
-        expect(events[0].get("tags")).to_not include("NO_ES")
-        #All clones go through filter_matched
-        expect(events[1].get("type")).to eq("nginx-access-clone1")
-        expect(events[1].get("tags")).to_not include("TESTLOG")
-        expect(events[1].get("tags")).to include("RABBIT")
-        expect(events[1].get("tags")).to include("NO_ES")
-
-        expect(events[2].get("type")).to eq("nginx-access-clone2")
-        expect(events[2].get("tags")).to_not include("TESTLOG")
-        expect(events[2].get("tags")).to include("RABBIT")
-        expect(events[2].get("tags")).to include("NO_ES")
-      end
-    end
-
-    describe "Bug LOGSTASH-1225" do
-      ### LOGSTASH-1225: Cannot clone events containing numbers.
-      let(:settings) { { "clones" => [ 'clone1' ] } }
-      let(:input) { { "type" => "bug-1225", "message" => "unused", "number" => 5 } }
-
-      it "clones events containing numbers" do
-        events = [event]
-        subject.filter(event) {|e| events << e }
-        expect(events[0].get("number")).to eq(5)
-        expect(events[1].get("number")).to eq(5)
-      end
-    end
-  end
-
-  describe "#register" do
-    context "when clones is an empty array" do
-      subject { described_class.new("clones" => []) }
-      it "logs a warning" do
-        expect(subject.logger).to receive(:warn)
-        expect { subject.register }.to_not raise_error
-      end
-    end
-    context "when clones is not set" do
-      subject { described_class.new }
-      it "raises an error" do
-        expect { subject.register }.to raise_error(ArgumentError)
+      context "when clones is not set" do
+        subject { described_class.new }
+        it "raises an error" do
+          expect { subject.register }.to raise_error(ArgumentError)
+        end
       end
     end
   end
